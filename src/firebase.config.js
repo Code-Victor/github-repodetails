7fcea2ff53b468dfeb8794fbf198907e50bb190a
Code-Victor <oluwaborihamzat@gmail.com>
2022-05-06 15:58:07 +0100

    added firebaseconfig for authentication

---
+++ src/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAht_krmGt5OUQqVyLedtX5ddi6wet946k",
+    authDomain: "github-repodetails.firebaseapp.com",
+    projectId: "github-repodetails",
+    storageBucket: "github-repodetails.appspot.com",
+    messagingSenderId: "558182501800",
+    appId: "1:558182501800:web:2178a32f85ae25f334619d"
+  };
