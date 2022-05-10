593ea7abb1c9826ccaf37a309569ac1546c85088
Code-Victor <oluwaborihamzat@gmail.com>
2022-05-11 00:39:42 +0100

    add the viewcontext provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import { useState } from 'react'
-import { AuthProvider, useFirebaseApp } from 'reactfire'
-import {getAuth} from 'firebase/auth'
-import {Routes,Route} from 'react-router-dom'
-import {Home,Login,Four04} from './pages'
-import { NavBar } from './components'
+import { useContext, useState } from "react";
+import { AuthProvider, useFirebaseApp } from "reactfire";
+import { getAuth } from "firebase/auth";
+import { Routes, Route } from "react-router-dom";
+import { Home, Login, Four04 } from "./pages";
+import { NavBar } from "./components";
+import { ViewContext } from "./context/viewContext";
 
 function App() {
-  const app= useFirebaseApp()
-  const auth= getAuth(app)
+  const app = useFirebaseApp();
+  const auth = getAuth(app);
+  const [inView,setInView]=useState(true)
   return (
-    <AuthProvider sdk={auth}>
-      <NavBar/>
-      <Routes>
-        <Route path="*" element={<Four04 />} />
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
-      </Routes>
-    </AuthProvider>
-  )
+    <ViewContext.Provider value={{inView,setInView }}>
+      <AuthProvider sdk={auth}>
+        <section className="bg-white">
+          <NavBar />
+          <Routes>
+            <Route path="*" element={<Four04 />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </section>
+      </AuthProvider>
+    </ViewContext.Provider>
+  );
 }
 
-export default App
+export default App;
