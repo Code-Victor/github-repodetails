a58f410ecf1eaa88ee771e6d9e274f4756d90c90
Code-Victor <oluwaborihamzat@gmail.com>
2022-05-11 00:47:00 +0100

    works with useinViewport hook

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { useInViewport } from "react-in-viewport";
+import { ViewContext } from "../context/viewContext";
+
+function Avatar({ src, className = "", main = false }) {
+  const { inView: item, setInView } = useContext(ViewContext);
+  const ref = useRef();
+  const { inViewport: inView } = useInViewport(ref);
+  if (main) {
+    setInView(inView);
+  }
+  useEffect(() => {
+    if (ref) {
+      console.log("view", ref.current);
+    }
+  });
+  return (
+    <img
+      src={src}
+      className={className + " w-20 h-20 rounded-full "}
+      ref={ref}
+    />
+  );
+}
+
+export default Avatar;
